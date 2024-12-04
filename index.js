2d77fa0e0732231eb3ed04d24b750640798ef698
nkopakbo <nazarii.kopak@bitsorchestra.com>
2024-12-05 00:00:04 +0200

    animation

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import * as THREE from "three";
+import gsap from "gsap";
 
 // Canvas
 const canvas = document.querySelector("#c");
@@ -6,80 +7,53 @@ const canvas = document.querySelector("#c");
 // Scene
 const scene = new THREE.Scene();
 
-/**
- * Objects
- */
-const group = new THREE.Group();
-scene.add(group);
-group.rotation.y = 0.4;
-group.rotation.x = 2;
-const cube1 = new THREE.Mesh(
-  new THREE.BoxGeometry(1, 1, 1),
-  new THREE.MeshBasicMaterial({ color: "#00ff00" }),
-);
-group.add(cube1);
+// Object
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
 
-const cube2 = new THREE.Mesh(
-  new THREE.BoxGeometry(1.5, 1.5, 1.5),
-  new THREE.MeshBasicMaterial({ color: "green" }),
-);
-cube2.position.x = 2;
-group.add(cube2);
-
-const cube3 = new THREE.Mesh(
-  new THREE.BoxGeometry(2, 2, 2),
-  new THREE.MeshBasicMaterial({ color: "blue" }),
-);
-cube3.position.x = 4;
-group.add(cube3);
-// const geometry = new THREE.BoxGeometry(1, 1, 1);
-// const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
-// const mesh = new THREE.Mesh(geometry, material);
-// mesh.position.x = 2;
-// mesh.position.y = -2;
-// mesh.position.z = 1;
-//The same as above - mesh.position.set(1.5, -2, 1);
-// mesh.scale.x = 2;
-// mesh.scale.set(0.5, 0.5, 0.5);
-// mesh.rotation.y = 4;
-// gimbal lock
-// mesh.rotation.reorder("YXZ"); //need to be before setting the rotation
-// mesh.rotation.x = Math.PI * 0.25;
-// mesh.rotation.set(2, -3, 1);
-//quaturnion
-
-// scene.add(mesh);
-
-// mesh.position.normalize(); // Normalize the position of the mesh
-
-const axesHelper = new THREE.AxesHelper();
-scene.add(axesHelper);
-/**
- * Sizes
- */
+// Sizes
 const sizes = {
-  width: 1024,
-  height: 720,
+  width: 800,
+  height: 600,
 };
 
-/**
- * Camera
- */
+// Camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
-// console.log(mesh.position.length(camera.position)); // Position of the camera
-camera.position.z = 5;
-// camera.position.x = 2;
-// camera.position.y = 1;
+camera.position.z = 3;
 scene.add(camera);
 
-// camera.lookAt(new THREE.Vector3(-2, 1, 0));
-// camera.lookAt(mesh.position);
-
-/**
- * Renderer
- */
+// Renderer
 const renderer = new THREE.WebGLRenderer({
   canvas: canvas,
 });
 renderer.setSize(sizes.width, sizes.height);
 renderer.render(scene, camera);
+
+// let time = Date.now();
+
+//Clock
+// const clock = new THREE.Clock();
+gsap.to(mesh.position, { x: 3, duration: 3, delay: 1 });
+gsap.to(mesh.position, { y: 2, duration: 2, delay: 0.5 });
+gsap.to(mesh.position, { z: -4, duration: 3, delay: 1 });
+
+//Animation
+const tick = () => {
+  // const currentTime = Date.now();
+  // const deltaTime = currentTime - time;
+  // time = currentTime;
+  // mesh.rotation.x += deltaTime * 0.001;
+  // const elapsedTime = clock.getElapsedTime(); //Pre built function to get elapsed time
+  // mesh.position.x = Math.cos(elapsedTime * 2);
+  // mesh.position.y = Math.sin(elapsedTime * 2);
+  // camera.lookAt(mesh.position);
+  // mesh.position.z = Math.cos(elapsedTime);
+  // mesh.position.z = elapsedTime * 0.1;
+
+  renderer.render(scene, camera);
+  window.requestAnimationFrame(tick);
+};
+
+tick();
