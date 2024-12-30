feede9cd56099b09c05b108d2300eb9d5e59654e
nkopakbo <nazarii.kopak@bitsorchestra.com>
2024-12-30 19:57:46 +0200

    first commit

--- index.js
+++ index.js
@@ -1,85 +1,118 @@
 import * as THREE from "three";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+//Array cameras allow multiple povs in the same screen (old mulitplayer games)
+//Stereo camera renders two images for VR like vision from 2 eyes (vr glasees, red/blue glasses ...)
+//Cube camera creates render of the surrounding from 6 povs (reflection, refraction, shadow)
+//Ortographic camera is used for 2D rendering, without any perspective
+//Perspective camera is used for 3D rendering, with perspective
+
+/**
+ * Base
+ */
+
+//Mouse move
+const cursor = {
+  x: 0,
+  y: 0,
+};
+
+window.addEventListener("mousemove", (e) => {
+  cursor.x = e.clientX / sizes.width - 0.5;
+  cursor.y = e.clientY / sizes.width - 0.5;
+});
 
 // Canvas
 const canvas = document.querySelector("#c");
 
+// Sizes
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
+window.addEventListener("resize", () => {
+  console.log("resize");
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize(sizes.width, sizes.height);
+
+  //Pixel ratio 1, 2, 3, 5
+  // More than 2 is just marketing, 2 is enough, eye cants see difference between 2,3,5
+  console.log(window.devicePixelRatio);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
+
+window.addEventListener("dblclick", () => {
+  const fullscreenElement =
+    document.fullscreenElement || document.webkitFullscreenElement;
+  if (!fullscreenElement) {
+    canvas.requestFullscreen();
+  } else {
+    document.exitFullscreen();
+  }
+});
+
 // Scene
 const scene = new THREE.Scene();
 
-/**
- * Objects
- */
-const group = new THREE.Group();
-scene.add(group);
-group.rotation.y = 0.4;
-group.rotation.x = 2;
-const cube1 = new THREE.Mesh(
-  new THREE.BoxGeometry(1, 1, 1),
-  new THREE.MeshBasicMaterial({ color: "#00ff00" }),
+// Object
+const mesh = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1, 5, 5, 5),
+  new THREE.MeshBasicMaterial({ color: 0xff0000 }),
 );
-group.add(cube1);
+scene.add(mesh);
 
-const cube2 = new THREE.Mesh(
-  new THREE.BoxGeometry(1.5, 1.5, 1.5),
-  new THREE.MeshBasicMaterial({ color: "green" }),
+// Camera
+//Parameters (pov degrees vertical fov, aspect ratio, near clipping, far clipping)
+//Anything between near and far clipping is rendered, anything outside is not
+const camera = new THREE.PerspectiveCamera(
+  75,
+  sizes.width / sizes.height,
+  1,
+  1000,
 );
-cube2.position.x = 2;
-group.add(cube2);
-
-const cube3 = new THREE.Mesh(
-  new THREE.BoxGeometry(2, 2, 2),
-  new THREE.MeshBasicMaterial({ color: "blue" }),
-);
-cube3.position.x = 4;
-group.add(cube3);
-// const geometry = new THREE.BoxGeometry(1, 1, 1);
-// const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
-// const mesh = new THREE.Mesh(geometry, material);
-// mesh.position.x = 2;
-// mesh.position.y = -2;
-// mesh.position.z = 1;
-//The same as above - mesh.position.set(1.5, -2, 1);
-// mesh.scale.x = 2;
-// mesh.scale.set(0.5, 0.5, 0.5);
-// mesh.rotation.y = 4;
-// gimbal lock
-// mesh.rotation.reorder("YXZ"); //need to be before setting the rotation
-// mesh.rotation.x = Math.PI * 0.25;
-// mesh.rotation.set(2, -3, 1);
-//quaturnion
-
-// scene.add(mesh);
-
-// mesh.position.normalize(); // Normalize the position of the mesh
-
-const axesHelper = new THREE.AxesHelper();
-scene.add(axesHelper);
-/**
- * Sizes
- */
-const sizes = {
-  width: 1024,
-  height: 720,
-};
-
-/**
- * Camera
- */
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
-// console.log(mesh.position.length(camera.position)); // Position of the camera
-camera.position.z = 5;
+// const camera = new THREE.OrthographicCamera(-1, 2, 1, -1, 0.1, 100);
 // camera.position.x = 2;
-// camera.position.y = 1;
+// camera.position.y = 2;
+camera.position.z = 4;
+camera.lookAt(mesh.position);
 scene.add(camera);
 
-// camera.lookAt(new THREE.Vector3(-2, 1, 0));
-// camera.lookAt(mesh.position);
+const controls = new OrbitControls(camera, canvas);
+// controls.enabled = false;
+controls.enableDamping = true;
 
-/**
- * Renderer
- */
+// Renderer
 const renderer = new THREE.WebGLRenderer({
   canvas: canvas,
 });
 renderer.setSize(sizes.width, sizes.height);
-renderer.render(scene, camera);
+
+// Animate
+const clock = new THREE.Clock();
+
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime();
+
+  // Update objects
+  // mesh.rotation.y = elapsedTime;
+  // camera.position.x = cursor.x * 3;
+  // camera.position.y = -cursor.y * 3;
+  // camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 3;
+  // camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 3;
+  // camera.position.y = cursor.y * 5;
+  // camera.lookAt(mesh.position);
+  // camera.lookAt(new THREE.Vector3());
+  controls.update();
+
+  // Render
+  renderer.render(scene, camera);
+
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick);
+};
+
+tick();
